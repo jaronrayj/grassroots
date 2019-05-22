41acdf3d5d9ec4c394bca794f7f43b8fa1270c38
dansirdan <daniel.monteton@gmail.com>
2019-05-22 11:57:25 -0600

    Firebase Auth.js created, old firebase references removed from login, config.json added to ignore

--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,7 @@
-$(document).ready(function() {
-  
-  $(document).on("submit", ".create-user", function(e) {
+$(document).ready(function () {
+  $(document).on("submit", ".create-user", function (e) {
     e.preventDefault();
-    
+
     var username = $("#username")
       .val()
       .trim();
@@ -12,7 +11,7 @@ $(document).ready(function() {
         "Please have at least 6 characters in length"
       );
     }
-  
+
     var email = $("#email")
       .val()
       .trim();
@@ -29,9 +28,7 @@ $(document).ready(function() {
       .trim();
 
     if (pwd !== pwdCheck) {
-      $("#password2-validate").text(
-        `These passwords do not match`
-      );
+      $("#password2-validate").text("These passwords do not match.");
     } else {
       var newUser = {
         user_name: username,
@@ -41,61 +38,12 @@ $(document).ready(function() {
 
       console.log(newUser);
 
-      
-
-      $.post("/api/users", newUser, function(data, status) {
+      $.post("/api/users", newUser, function (data, status) {
         // alert("Data: " + data + "\nStatus: " + status);
         console.log(status);
         location.replace("/projects");
       });
-
-      // FIREBASE SIGN UP NEW USER
-      firebase.auth().createUserWithEmailAndPassword(email, pwd).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        console.log("ERROR CODE " + errorCode + ": " + errorMessage);
-      });
     }
-
   });
   // $(document).on("submit", ".login", loginUser);
-
-  // FIREBASE LOGIN EXISTING USER
-  firebase.auth().signInWithEmailAndPassword(email, pwd).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log("ERROR CODE " + errorCode + ": " + errorMessage);
-  });
-
-  // AUTHENTICATION STATE OBSERVER
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-      // var displayName = user.displayName;
-      // var email = user.email;
-      // var emailVerified = user.emailVerified;
-      // var photoURL = user.photoURL;
-      // var isAnonymous = user.isAnonymous;
-      // var uid = user.uid;
-      // var providerData = user.providerData;
-      // ...
-    } else {
-      // User is signed out.
-      // ...
-    }
-  });
-
-  // FIREBASE LOGOUT STATE CHANGE
-  $("#logout").on("click", function(e) {
-    e.stopPropagation();
-
-    auth.signOut().then(function() {
-      console.log("logged out");
-    }).catch(function(error) {
-      console.log(error);
-    });
-  });
-
 });
