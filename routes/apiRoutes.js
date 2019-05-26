c66c32e31018a0225dfe711dc63d82513b4ba590
dansirdan <daniel.monteton@gmail.com>
2019-05-26 11:04:54 -0600

    able to grab the current logged in user's id

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,6 +10,7 @@ module.exports = function (app) {
     // So we're sending the user back the route to the members page because the redirect will happen on the front end
     // They won't get this or even be able to access this page if they aren't authed
     res.json("/projects");
+    console.log(req.session.passport.user.id);
   });
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
